cd7f20394cb82cc768ec35af725c8e9fbc75444c
jmoswalt <jmoswalt@gmail.com>
2012-12-05 11:53:55 -0600

    Adding license

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 longdesc = \
 '''
-An addon for Tendenci for displaying videos.
+An addon for Tendenci for displaying embedded videos.
 '''
 
 setup(
@@ -10,7 +10,7 @@ setup(
     author='John-Michael Oswalt',
     author_email='jmoswalt@schipul.com',
     version='1.0.2',
-    license='',
+    license='GPL3',
     description='An addon for Tendenci for displaying embedded videos.',
     long_description=longdesc,
     url='https://github.com/tendenci/tendenci-videos',
