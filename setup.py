b52bed678307aa044b664f78485aa7469c9d6913
jmoswalt <jmoswalt@gmail.com>
2012-12-05 15:42:30 -0600

    Updating author, email, and version number.

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ An addon for Tendenci for displaying embedded videos.
 
 setup(
     name='tendenci-videos',
-    author='John-Michael Oswalt',
-    author_email='jmoswalt@schipul.com',
-    version='1.0.2',
+    author='Schipul',
+    author_email='programmers@schipul.com',
+    version='1.0.3',
     license='GPL3',
     description='An addon for Tendenci for displaying embedded videos.',
     long_description=longdesc,
