319a8885eb2d8dce09033bd0c587326e0091f753
bpotterschipul <bpotter@schipul.com>
2013-04-29 16:37:20 -0500

    Adding ability to change label and URL for module
    Signed-off-by: bpotterschipul <bpotter@schipul.com>

--- videos/urls.py
+++ videos/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls.defaults import *
 from videos.feeds import LatestEntriesFeed
+from tendenci.core.site_settings.utils import get_setting
+
+urlpath = get_setting('module', 'videos', 'url')
+if not urlpath:
+    urlpath = "videos"
 
 urlpatterns = patterns('videos.views',
-    url(r'^videos/$', 'search', name="video"),
-    url(r'^videos/category/([^/]+)/$', 'index', name="video.category"),
-    url(r'^videos/search/$', 'search', name="video.search"),
-    url(r'^videos/feed/$', LatestEntriesFeed(), name='video.feed'),
-    url(r'^videos/([^/]+)/$', 'detail', name="video.details"),
+    url(r'^%s/$' % urlpath, 'search', name="video"),
+    url(r'^%s/category/([^/]+)/$'  % urlpath, 'index', name="video.category"),
+    url(r'^%s/search/$' % urlpath, 'search', name="video.search"),
+    url(r'^%s/feed/$' % urlpath, LatestEntriesFeed(), name='video.feed'),
+    url(r'^%s/([^/]+)/$' % urlpath, 'detail', name="video.details"),
 )
