f82c86c227d188c7b8e705befc7196e81e7d6dda
JMO <jmoswalt@gmail.com>
2013-04-29 16:43:06 -0500

    Version bump for urls settings

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='tendenci-videos',
     author='Schipul',
     author_email='programmers@schipul.com',
-    version='1.0.5',
+    version='1.0.6',
     license='GPL3',
     description='Videos addon for Tendenci',
     long_description=longdesc,
