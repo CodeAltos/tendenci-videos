884e7ab8164fb449e3019dcdedf35de18ae92002
jmoswalt <jmoswalt@gmail.com>
2013-07-12 09:02:25 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='tendenci-videos',
     author='Schipul',
     author_email='programmers@schipul.com',
-    version='1.0.9',
+    version='1.0.10',
     license='GPL3',
     description='Videos addon for Tendenci',
     long_description=longdesc,
