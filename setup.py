3076ae50361ca32bea111c40995a0f5714281c2d
jmoswalt <jmoswalt@gmail.com>
2013-07-12 08:59:50 -0500

    Squashed commit of the following:
    commit 7e011eb0e65f3e810c028908f8c919bffeb0b722
    Author: John Eddie Ayson <je@bixly.com>
    Date:   Fri Jul 12 16:11:27 2013 +0800
    
        #39: Video suggestions on video

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='tendenci-videos',
     author='Schipul',
     author_email='programmers@schipul.com',
-    version='1.0.8',
+    version='1.0.9',
     license='GPL3',
     description='Videos addon for Tendenci',
     long_description=longdesc,
