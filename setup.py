9f44a3c0d7270c232dd65c15a3d75998a485821e
Jenny Qian <jqian@schipul.com>
2014-01-14 15:41:33 -0600

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='tendenci-videos',
     author='Schipul',
     author_email='programmers@schipul.com',
-    version='1.0.13',
+    version='1.0.14',
     license='GPL3',
     description='Videos addon for Tendenci',
     long_description=longdesc,
